6eeafb9996d2b260c8a97e9910abae97c71342a2
yosefther <yousefthair9@gmail.com>
2026-06-05 20:43:12 +0300

    Wire todo routes to controller handlers

--- backend/routes/web.go
+++ backend/routes/web.go
@@ -7,7 +7,8 @@ import (
 func Init() *mux.Router{
 	router := mux.NewRouter()
 	router.HandleFunc("/" , controllers.Show)
-	router.HandleFunc("/add" , controllers.Add).Method("POST")
+	router.HandleFunc("/add" , controllers.Add).Methods("POST")
 	router.HandleFunc("/delete/{id}" , controllers.Delete)
 	router.HandleFunc("/complete/{id}" , controllers.Complete)
+	return router
 }
